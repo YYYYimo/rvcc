90c0149c51c2c0704f34a44d32c3aab030aee2c5
YYYYimo <2623855900@qq.com>
2024-07-26 19:16:01 +0800

    prepare main.c and rvcc.h

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "./rvcc.h"
+
+int main(int Argc, char **Argv)
+{
+    if (Arg != 2)
+    {
+        error("%s: Invalid number of args", Argv[0]);
+    }
+
+    //解析Argv[1], 生成终结符流
+    Token *Tok = tokenize(Argv[1]);
+
+    //解析终结符流
+    Node *Nd = parse(Tok);
+
+    //生成代码
+    codegen(Nd);
+
+    return 0;
+}
---
+++ rvcc.h
@@ -0,0 +1,66 @@
+#include <assert.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum 
+{
+    TK_PUNCT,
+    TK_NUM,
+    TK_EOF
+} TokenKind;
+
+typedef struct Token Token;
+struct Token
+{
+    TokenKind Kind;
+    Token *Next;
+    int Val;
+    char *Loc;
+    int Len;
+};
+
+//报错函数
+void error(char *Fmt, ...);
+void errorAt(char *Loc, char *Fmt, ...);
+void errorTok(Token *Tok, char *Fmt, ...);
+
+bool equal(Token *Tok, char *Str);
+Token *skip(Token *Tok, char *Str);
+//词法分析
+Token *tokenize(char *Input);
+
+//抽象语法树部分
+//AST节点类型
+typedef enum
+{
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NEG,
+    ND_NE,
+    ND_EQ,
+    ND_LT,
+    ND_LE,
+    ND_NUM
+} NodeKind;
+
+//AST中二叉树节点
+typedef struct Node Node;
+struct Node
+{
+    NodeKind Kind;
+    Node *LHS;
+    Node *RHS;
+    int Val;
+};
+
+//语法分析与代码生成
+Node *parse(Token *Tok);
+
+//代码生成入口函数
+void codegen(Node *Nd);
